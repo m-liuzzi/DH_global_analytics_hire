974b1d47587e97ab0aa8111dbfb58af110a26493
m-liuzzi <38541986+m-liuzzi@users.noreply.github.com>
2020-08-18 12:31:06 +0200

    Update and rename test_python_answers to test_python_answers.py

--- test_python_answers.py
+++ test_python_answers.py
@@ -5,11 +5,11 @@ import unittest
 class test_python_answers(unittest.TestCase):
   def test_read_data(self):
     df = read_data('https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=BTC&market=USD&apikey=NQW2B7EP84XGZ82L')
-    self.assertTrue(len(df)) > 0
+    self.assertIsInstance(df, pd.DataFrame)
     
   def test_transform_df(self):
     df = transform_df(df)
-    self.assertTrue(len(df)) > 0
+    self.assertTrue(len(df.columns)) == 2
     
   def test_calculate_weekly_price(self):
     df = calculate_weekly_price(df)
@@ -23,4 +23,9 @@ class test_python_answers(unittest.TestCase):
     df = calculate_rolling_7(df)
     self.assertTrue(len(df)) > 0
  
-unittest.main(argv=[''],verbosity=2, exit=False)
+if __name__ == “__main__“:
+  test_read_data()
+  test_transform_df()
+  test_calculate_weekly_price
+  test_calculate_rolling_3
+  test_calculate_rolling_7
