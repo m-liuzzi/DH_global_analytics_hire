a58ff8133c6336e2bfc77b73fa0a8d2ff9517186
m-liuzzi <38541986+m-liuzzi@users.noreply.github.com>
2020-08-18 12:35:45 +0200

    Update test_python_answers.py

--- test_python_answers.py
+++ test_python_answers.py
@@ -1,31 +1,30 @@
 # -*- coding: utf-8 -*-
 from python_questions_and_answers import *
-import unittest
 
-class test_python_answers(unittest.TestCase):
-  def test_read_data(self):
-    df = read_data('https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=BTC&market=USD&apikey=NQW2B7EP84XGZ82L')
-    self.assertIsInstance(df, pd.DataFrame)
-    
-  def test_transform_df(self):
-    df = transform_df(df)
-    self.assertTrue(len(df.columns)) == 2
-    
-  def test_calculate_weekly_price(self):
-    df = calculate_weekly_price(df)
-    self.assertTrue(len(df)) > 0
-  
-  def test_calculate_rolling_3(self):
-    df = calculate_rolling_3(df)
-    self.assertTrue(len(df)) > 0
-    
-  def test_calculate_rolling_7(self):
-    df = calculate_rolling_7(df)
-    self.assertTrue(len(df)) > 0
+def test_read_data():
+  df = read_data('https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=BTC&market=USD&apikey=NQW2B7EP84XGZ82L')
+  self.assertIsInstance(df, pd.DataFrame)
+  return df
+
+def test_transform_df(df):
+  df = transform_df(df)
+  self.assertTrue(len(df.columns)) == 2
+
+def test_calculate_weekly_price(df):
+  df = calculate_weekly_price(df)
+  self.assertTrue(len(df)) > 0
+
+def test_calculate_rolling_3(df):
+  df = calculate_rolling_3(df)
+  self.assertTrue(len(df)) > 0
+
+def test_calculate_rolling_7(df):
+  df = calculate_rolling_7(df)
+  self.assertTrue(len(df)) > 0
  
 if __name__ == “__main__“:
   test_read_data()
-  test_transform_df()
-  test_calculate_weekly_price()
-  test_calculate_rolling_3()
-  test_calculate_rolling_7()
+  test_transform_df(df)
+  test_calculate_weekly_price(df)
+  test_calculate_rolling_3(df)
+  test_calculate_rolling_7(df)
